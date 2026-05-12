01544767ae24881f278b6c646fd057b92358b3a2
elmokhtarim <mohamed.elmokhtari26@gmail.com>
2026-05-12 11:17:03 +0200

    feat : add asyncio event loop in dedicated thread

--- src/pymodaq_plugins_arduino/hardware/esp32_telemetrix.py
+++ src/pymodaq_plugins_arduino/hardware/esp32_telemetrix.py
@@ -9,3 +9,7 @@ class ArduinoWifi:
     def __init__(self, ip_address):
         self.pin_values_output = {}
         self.analog_pin_values_input = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+
+        self._loop = asyncio.new_event_loop()
+        self._thread = threading.Thread(target=self._loop.run_forever, daemon=True)
+        self._thread.start()
