6b367e413d7c73a83b7946725daf5e345b2780ed
elmokhtarim <mohamed.elmokhtari26@gmail.com>
2026-05-12 12:10:32 +0200

    feat : add get_actuator_value, close, commit_settings

--- src/pymodaq_plugins_arduino/daq_move_plugins/daq_move_FanHeater.py
+++ src/pymodaq_plugins_arduino/daq_move_plugins/daq_move_FanHeater.py
@@ -1,19 +1,25 @@
 from typing import Optional
+
 from pymodaq.control_modules.move_utility_classes import (DAQ_Move_base, comon_parameters_fun, main,
                                                           DataActuatorType, DataActuator)
+from pymodaq_utils.utils import ThreadCommand
 from pymodaq_gui.parameter import Parameter
+
 from pymodaq_plugins_arduino.hardware.esp32_telemetrix import ArduinoWifi
 from pymodaq_plugins_arduino.utils import Config
 
 config = Config()
 
+
 class DAQ_Move_FanHeater(DAQ_Move_base):
     """Plugin PyMoDAQ pour le contrôle du chauffage et du ventilateur
     via XY-MOS PWM sur ESP32 en WiFi (Telemetrix).
+
     Broches ESP32 :
         Chauffage   → GPIO18
         Ventilateur → GPIO17
     """
+
     _controller_units = '%'
     is_multiaxes = True
     _axis_names = {
@@ -24,46 +30,12 @@ class DAQ_Move_FanHeater(DAQ_Move_base):
     data_actuator_type = DataActuatorType['DataActuator']
 
     params = [
-                 {'title': 'IP Address:', 'name': 'ip_address', 'type': 'str',
-                  'value': config('esp32', 'ip_address')}
-             ] + comon_parameters_fun(is_multiaxes, axis_names=_axis_names, epsilon=_epsilon)
+        {'title': 'IP Address:', 'name': 'ip_address', 'type': 'str',
+         'value': config('esp32', 'ip_address')}
+    ] + comon_parameters_fun(is_multiaxes, axis_names=_axis_names, epsilon=_epsilon)
 
     def ini_attributes(self):
         self.controller: Optional[ArduinoWifi] = None
-
-    def ini_stage(self, controller=None):
-        self.controller = self.ini_stage_init(
-            old_controller=controller,
-            new_controller=None)
-        if self.is_master:
-            self.controller = ArduinoWifi(
-                ip_address=self.settings['ip_address']
-            )
-            self.set_pins()
-        info = "Heater and Fan ready"
-        initialized = True
-        return info, initialized
-
-    def set_pins(self):
-        for pin in self._axis_names.values():
-            self.controller.set_pin_mode_analog_output(pin)
-
-    def ini_stage(self, controller=None):
-        self.controller = self.ini_stage_init(
-            old_controller=controller,
-            new_controller=None)
-        if self.is_master:
-            self.controller = ArduinoWifi(
-                ip_address=self.settings['ip_address']
-            )
-            self.set_pins()
-        info = "Heater and Fan ready"
-        initialized = True
-        return info, initialized
-
-    def set_pins(self):
-        for pin in self._axis_names.values():
-            self.controller.set_pin_mode_analog_output(pin)
 
     def get_actuator_value(self):
         pos = DataActuator(data=self.controller.get_output_pin_value(self.axis_value))
@@ -77,6 +49,25 @@ class DAQ_Move_FanHeater(DAQ_Move_base):
 
     def commit_settings(self, param: Parameter):
         pass
+
+    def ini_stage(self, controller=None):
+        self.controller = self.ini_stage_init(
+            old_controller=controller,
+            new_controller=None)
+
+        if self.is_master:
+            self.controller = ArduinoWifi(
+                ip_address=self.settings['ip_address']
+            )
+            self.set_pins()
+
+        info = "Heater and Fan ready"
+        initialized = True
+        return info, initialized
+
+    def set_pins(self):
+        for pin in self._axis_names.values():
+            self.controller.set_pin_mode_analog_output(pin)
 
     def move_abs(self, value: DataActuator):
         value = self.check_bound(value)
@@ -98,8 +89,6 @@ class DAQ_Move_FanHeater(DAQ_Move_base):
     def stop_motion(self):
         pass
 
+
 if __name__ == '__main__':
     main(__file__)
-
-
-
