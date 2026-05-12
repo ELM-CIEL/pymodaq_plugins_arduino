e771dc46b0397c4b7a8273604354b92cbb09bddf
elmokhtarim <mohamed.elmokhtari26@gmail.com>
2026-05-12 11:19:23 +0200

    feat : add board initialization via asyncio

--- src/pymodaq_plugins_arduino/hardware/esp32_telemetrix.py
+++ src/pymodaq_plugins_arduino/hardware/esp32_telemetrix.py
@@ -13,3 +13,10 @@ class ArduinoWifi:
         self._loop = asyncio.new_event_loop()
         self._thread = threading.Thread(target=self._loop.run_forever, daemon=True)
         self._thread.start()
+
+    async def _init_board(self, ip_address):
+        self._board = telemetrix_aio_esp32.TelemetrixAioEsp32(
+            transport_address=ip_address,
+            autostart=True,
+            loop=self._loop
+        )
