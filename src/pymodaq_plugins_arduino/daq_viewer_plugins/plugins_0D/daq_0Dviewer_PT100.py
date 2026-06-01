79660adb191225e3b9c6bde7b99e6d441abdc1c5
elmokhtarim <mohamed.elmokhtari26@gmail.com>
2026-06-01 09:59:51 +0200

    Fix update import path to spi_max31865

--- src/pymodaq_plugins_arduino/daq_viewer_plugins/plugins_0D/daq_0Dviewer_PT100.py
+++ src/pymodaq_plugins_arduino/daq_viewer_plugins/plugins_0D/daq_0Dviewer_PT100.py
@@ -6,7 +6,7 @@ from pymodaq.control_modules.viewer_utility_classes import DAQ_Viewer_base, como
 from pymodaq.utils.parameter import Parameter
 
 from pymodaq_plugins_arduino.hardware.esp32_telemetrix import ArduinoWifi
-from pymodaq_plugins_arduino.hardware.sensors.max31865.esp32_telemetrix_max31865 import MAX31865
+from pymodaq_plugins_arduino.hardware.sensors.max31865.spi_max31865 import MAX31865
 from pymodaq_plugins_arduino.utils import Config
 
 config = Config()
